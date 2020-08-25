b83b6c308bebdb1a78b8c5a26da2cfad14f61cae
Juliano Santos <juliano.sntos@hotmail.com>
2020-08-25 15:51:41 -0300

    V.2

---
+++ Conta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace START_UNI
+{
+    public abstract class Conta
+    {
+        public abstract void Saca(double valor);
+    }
+
+    public class ContaCorrente : Conta
+    {
+        public double Saldo { get; private set; }
+
+        public override void Saca(double valor)
+        {
+            this.Saldo -= (valor * 0.03);
+        }
+       
+    }
+
+    public class ContaPoupanca : Conta
+    {
+        public double Saldo { get; private set; }
+
+        public override void Saca(double valor)
+        {
+            this.Saldo -= (valor * 0.05 );
+        }
+      
+    }
+}
