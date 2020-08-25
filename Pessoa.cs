070db04001b81d01e6c6373aeadec93f808c6cb4
Juliano Santos <juliano.sntos@hotmail.com>
2020-08-25 15:37:03 -0300

    V.1

---
+++ Pessoa.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace START_UNI
+{
+    public class Pessoa
+    {
+        private String nome;
+        private String endereco;
+        private String telefone;
+
+        public Pessoa(String nome, String endereco, String telefone)
+        {
+            this.nome = nome;
+            this.endereco = endereco;
+            this.telefone = telefone;
+        }
+
+        public Pessoa()
+        {
+        }
+
+        public String getNome()
+        {
+            return this.nome;
+        }
+
+        public void setNome(String nome)
+        {
+            this.nome = nome;
+        }
+
+        public String getEndereco()
+        {
+            return this.endereco;
+        }
+
+        public void setEndereco(String endereco)
+        {
+            this.endereco = endereco;
+        }
+
+        public String getTelefone()
+        {
+            return this.telefone;
+        }
+
+        public void setTelefone(String telefone)
+        {
+            this.telefone = telefone;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace START_UNI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int start;
+
+            //do
+            //{
+            //    start = Convert.ToInt32(Console.ReadLine());
+
+            //} while (start > 0);
+
+            //int end = Convert.ToInt32(Console.ReadLine());
+
+            //for (int x = start; x <= end; x++)
+            //{
+            //    if (x % 2 != 0)
+            //    {
+            //        Console.WriteLine(x);
+            //    }
+            //}
+
+            Pessoa p1 = new Pessoa();
+            p1.setNome("Juliano");
+            p1.setEndereco("Major Joaquim Silverio");
+            p1.setTelefone("15991714721");
+
+            Console.WriteLine("Nome:" + p1.getNome());
+            Console.WriteLine("Endereco:" + p1.getEndereco());
+            Console.WriteLine("Telefone:" + p1.getTelefone());
+
+        }
+    }
+}
